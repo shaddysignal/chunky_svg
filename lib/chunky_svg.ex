40240cc66036e50eb6fbc48792e2eb41223e2c75
Anton Shubin <signal2564@gmail.com>
2017-08-29 03:30:25 +0300

    new custom first tuple for rendering

--- lib/chunky_svg.ex
+++ lib/chunky_svg.ex
@@ -4,13 +4,18 @@ defmodule ChunkySVG do
   end
 
   def render(drawing) when is_list(drawing) do
+    _render(%{viewBox: "0 0 100 100", xmlns: "http://www.w3.org/2000/svg"}, drawing)
+  end
+
+  def render(svg_attrs, drawing) when is_tuple(svg_attrs) and is_list(drawing) do
+    _render(svg_attrs, drawing)
+  end
+
+  defp _render(svg_attrs, drawing) do
     drawing = drawing |>
       ChunkySVG.Builtin.expand |>
       ChunkySVG.InlineMacro.expand
-    {
-      :svg,
-      %{viewBox: "0 0 100 100", xmlns: "http://www.w3.org/2000/svg"},
-      drawing
-    } |> XmlBuilder.generate
+
+    {:svg, svg_attrs, drawing} |> XmlBuilder.generate
   end
 end
